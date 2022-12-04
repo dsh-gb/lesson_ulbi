3cc0ef712fc81c32eacd84e2c1fd050d900d1ddf
dsh-gb <shamonov.dima@gmail.com>
2022-12-04 22:34:16 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import ClassCounter from './components/ClassCounter';
+import Counter from './components/Counter';
+import './styles/App.css';
+
+function App() {
+
+  return (
+    <div className="App">
+      <div className='post'>
+        <div className='post__content'>
+          <strong>1. JavaScript</strong>
+          <div>
+            JavaScript - first programming lang
+          </div>
+        </div>
+        <div className='post__btns'>
+          <button>Delete</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
